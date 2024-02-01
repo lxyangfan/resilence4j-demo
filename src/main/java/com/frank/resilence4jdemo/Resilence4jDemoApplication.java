51cb90deec070755540b5a083dc96fa768022cbe
Fan Yang <frank.yang@miotech.com>
2024-02-01 16:08:51 +0800

    init

---
+++ src/main/java/com/frank/resilence4jdemo/Resilence4jDemoApplication.java
@@ -0,0 +1,13 @@
+package com.frank.resilence4jdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilence4jDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilence4jDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frank/resilence4jdemo/Resilence4jDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frank.resilence4jdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilence4jDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
